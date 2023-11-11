1e6a0d63c904bbf8e303ab4f9924fafb3976bca0
SalimAbdessamad <samimalim1991@gmail.com>
2023-11-11 08:54:42 -0800

    update file _printf.c

--- _printf.c
+++ _printf.c
@@ -4,36 +4,7 @@
  * @format: --
  * Return: --
  */
-int _printf(const char * const format, ...)
+int _printf(const char *format, ...)
 {
-convert tab[] = {
-{"%s", print_s}, {"%c", print_c},
-{"%%", print_37ASCII},
-{"%i", print_i}, {"%d", print_d}
-};
-va_list args;
-int i = 0, j, len = 0;
-va_start(args, format);
-if (format == NULL || (format[0] == '%' && format[1] == '\0'))
-return (-1);
-Here:
-while (format[i] != '\0')
-{
-j = 4;
-while (j >= 0)
-{
-if (tab[j].pch[0] == format[i] && tab[j].pch[1] == format[i + 1])
-{
-len += tab[j].function(args);
-i = i + 2;
-goto Here;
-}
-j--;
-}
-_putchar(format[i]);
-len++;
-i++;
-}
-va_end(args);
-return (len);
+
 }
