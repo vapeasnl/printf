93d4dd48026022856c7be2369e1c173b2841d424
SalimAbdessamad <samimalim1991@gmail.com>
2023-11-14 14:04:06 -0800

    test printf

--- _printf.c
+++ _printf.c
@@ -1,44 +1,44 @@
-#include "main.h"
+#include "holberton.h"
 /**
- * _printf - printf input and specifier
- * @format: input
- * Return: len or error
+ * _printf - is a function that selects the correct function to print.
+ * @format: identifier to look for.
+ * Return: the length of the string.
  */
-int _printf(const char *format, ...)
+int _printf(const char * const format, ...)
 {
+	convert_match m[] = {
+		{"%s", printf_string}, {"%c", printf_char},
+		{"%%", printf_37},
+		{"%i", printf_int}, {"%d", printf_dec}, {"%r", printf_srev},
+		{"%R", printf_rot13}, {"%b", printf_bin}, {"%u", printf_unsigned},
+		{"%o", printf_oct}, {"%x", printf_hex}, {"%X", printf_HEX},
+		{"%S", printf_exclusive_string}, {"%p", printf_pointer}
+	};
+
 	va_list args;
-	unsigned int i, len = 0;
+	int i = 0, j, len = 0;
 
 	va_start(args, format);
-
-	if (!format || (format[0] == '%' && !format[1]))
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
-	for (i = 0; format[i]; i++)
+
+Here:
+	while (format[i] != '\0')
 	{
-		if (format[i] == '%')
+		j = 13;
+		while (j >= 0)
 		{
-			if (format[i + 1] == '%')
+			if (m[j].id[0] == format[i] && m[j].id[1] == format[i + 1])
 			{
-				_putchar('%');
-				i++;
-				len++;
+				len += m[j].f(args);
+				i = i + 2;
+				goto Here;
 			}
-			else if (prec(format, i + 1) != NULL)
-			{
-				len += prec(format, i + 1)(args);
-				i++;
-			}
-			else
-			{
-				_putchar(format[i]);
-				len++;
-			}
+			j--;
 		}
-		else
-		{
-			_putchar(format[i]);
-			len++;
-		}
+		_putchar(format[i]);
+		len++;
+		i++;
 	}
 	va_end(args);
 	return (len);
--- _putchar.c
+++ _putchar.c
@@ -1,8 +1,11 @@
-#include "main.h"
+#include "holberton.h"
+
 /**
- * _putchar - writes the character c
+ * _putchar - writes the character c to stdout
  * @c: The character to print
- * Return: On success 1, -1  on error
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c)
 {
