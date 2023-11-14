bb09b5ec216c5f7e1d75e2fc6afabff030840ddd
Ahmed <114778876+ShinRwiz@users.noreply.github.com>
2023-11-14 01:49:29 +0100

    binary fixs

--- _printf.c
+++ _printf.c
@@ -2,6 +2,7 @@
 /**
  * _printf - printf input and spec
  * @format: input
+ *
  * Return: len or error
  */
 int _printf(const char *format, ...)
@@ -13,30 +14,26 @@ int _printf(const char *format, ...)
 
 	if (!format || (format[0] == '%' && !format[1]))
 		return (-1);
-	for (i = 0; format[i]; i++)
+	for (i = 0; format[i]; i++) /*runs along the string*/
 	{
 		if (format[i] == '%')
 		{
 			if (format[i + 1] == '%')
-			{
-				_putchar('%');
+			{   _putchar('%');
 				i = i + 1;
 				len++;
 			}
 			else if (prec(format, i + 1) != NULL)
-			{
-				len += prec(format, i + 1)(args);
+			{   len += prec(format, i + 1)(args);
 				i = i + 1;
 			}
 			else
-			{
-				_putchar(format[i]);
+			{ _putchar(format[i]);
 				len++;
 			}
 		}
 		else
-		{
-			_putchar(format[i]);
+		{ _putchar(format[i]);
 			len++;
 		}
 	}
--- _putchar.c
+++ _putchar.c
@@ -2,8 +2,9 @@
 /**
  * _putchar - writes the character c
  * @c: The character to print
- * Return: On success 1, -1  on error
- */
+ * Return: On success 1
+ * On error, return (-1), and error is set appropriately.
+*/
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
